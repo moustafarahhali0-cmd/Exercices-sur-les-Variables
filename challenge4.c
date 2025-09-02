043a20a0630b4f97c879b73e1a7de76f85121ebe
moustafarahhali0-cmd <moustafarahhali0@gmail.com>
2025-09-02 22:00:53 +0200

    challenge4.c

--- challenge4.c
+++ challenge4.c
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
 int main()
@@ -13,8 +6,8 @@ int main()
     float kmh,ms;
     printf("Entrez la vitesse en km/h : ");
     scanf("%f",&kmh);
-    ms=kmh/3.6;
-    printf("%.2f kmh=== %.2f ms",kmh,ms);
+    ms = kmh × 0.27778
+    printf("%.2f kmh = %.2f ms",kmh,ms);
 
 
     return 0;
