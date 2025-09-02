0cd9df954f0879388eea379eacfbbf2999d52183
moustafarahhali0-cmd <moustafarahhali0@gmail.com>
2025-09-02 22:02:56 +0200

    challenge5.c

--- challenge5.c
+++ challenge5.c
@@ -1,22 +1,15 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
-    int temperature;
+    int t;
      printf("Entrez la temperature en Celsius : ");
-     scanf("%d", &temperature);
-     if(temperature<0){
+     scanf("%d", &t);
+     if(t<0){
          printf("glace");
      }
-     else if(temperature>=0&&temperature<100){
+     else if(t>=0&&t<100){
          printf("liquide");
      }
      else{
