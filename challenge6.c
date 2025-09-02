b2c8c3b5cb07452340fe8da45b05fc7c698f407d
moustafarahhali0-cmd <moustafarahhali0@gmail.com>
2025-09-02 22:09:38 +0200

    challenge6.c

--- challenge6.c
+++ challenge6.c
@@ -3,24 +3,12 @@
 
 int main()
 {
-    int A,B,operateur;
-    printf("enter number one ?\n");
+    int A,B;
+    printf("enter valuer A ?\n");
     scanf("%d",&A);
-    printf("enter number two ?\n");
+    printf("enter valuer B?\n");
     scanf("%d",&B);
-    printf("plase choice operateur:\n1:(+)\n2:(-)\n3(*)\n");
-    scanf("%d",&operateur);
-    switch(operateur){
-        case 1:printf("%d + %d = %d ",A,B,A+B);
-        break;
-        case 2:printf("%d - %d = %d ",A,B,A-B);
-        break;
-        case 3:printf("%d x %d = %d ",A,B,A*B);
-        break;
-        default:printf("invalid choice");
-        break;
-        
-    }
+    printf(" A +  B = %d ",A+B);
     
     
     
