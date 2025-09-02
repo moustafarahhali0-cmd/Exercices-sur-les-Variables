c038d5c9a2dd538adfba8ccbe8685b026d5d11dc
moustafarahhali0-cmd <moustafarahhali0@gmail.com>
2025-09-02 21:54:08 +0200

    challenge2.c

--- challenge2.c
+++ challenge2.c
@@ -1,22 +1,15 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 
 int main()
 {    
 
-    float celsius;
-    float kelvin;
+    float C;
+    float K;
     printf("Entrez la temperature en Celsius? : ");
-    scanf("%f",&celsius);
-    kelvin=273.15+celsius;
-    printf("%.2f c== %.2f k",celsius,kelvin);
+    scanf("%f",&C);
+    K = C + 273.15
+    printf("%.2f c== %.2f k",C,K);
 
 
     return 0;
