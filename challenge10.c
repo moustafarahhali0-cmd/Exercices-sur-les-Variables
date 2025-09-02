a1f1f35a0e00a22208795ef755d3b513aebf42f7
moustafarahhali0-cmd <moustafarahhali0@gmail.com>
2025-09-02 23:47:44 +0200

    challenge10.c

--- challenge10.c
+++ challenge10.c
@@ -1,21 +1,16 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <math.h>
 
 int main()
 {
-        float radius, volume;
-        printf("Enter the radius of the sphere: ");
-        scanf("%f", &radius);
-        volume = (4.0 / 3.0) * M_PI * pow(radius, 3);
-        printf("the valume the radius of the sphere %.2f is %.2f",radius,volume);
+        float r, volume;
+        float const pi=3.14;
+        printf("Enter la rayon de sphere ? ");
+        scanf("%f", &r);
+        volume = (4 / 3) * pi * pow(r, 3);
+        printf("the valume the rayon de sphere est  %.2f",volume);
+        
         
 
 
