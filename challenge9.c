ad2718bc4a23825878dd93ba0293f1ff76e55168
moustafarahhali0-cmd <moustafarahhali0@gmail.com>
2025-09-02 23:17:36 +0200

    challenge9.c

--- challenge9.c
+++ challenge9.c
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <math.h>
 
@@ -13,13 +6,13 @@ int main()
 {
     float x1, y1, z1;
     float x2, y2, z2;
-    float distance;
-    printf("Enter coordinates of point A (x1 y1 z1): ");
+    float d;
+    printf("Enter distance euclidienne (x1 y1 z1): ");
     scanf("%f %f %f", &x1, &y1, &z1);
-    printf("enter coordinates of point B (x2 y2 z3):");
-    scanf("%f%f%f",&x1,&y2,&z2);
+    printf("enter distance euclidienne (x2 y2 z3):");
+    scanf("%f%f%f",&x2,&y2,&z2);
     distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) + pow(z2 - z1,2));
-    printf("les distance est : %.f",distance);
+    printf("les distance est : %.f",d);
 
 
     return 0;
